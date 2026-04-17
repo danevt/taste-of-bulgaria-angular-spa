82de1de7b40b75fa73766c0183f932b843cc11fe
T.Danev <danevtm@gmail.com>
2026-04-18 00:39:25 +0300

    Update header logic: add router navigation on logout and sticky scroll behavior

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+// src/app/shared/components/header/header.ts
+import { Component, inject, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  protected authService = inject(AuthService);
+  private router = inject(Router);
 
+  isLoggedIn = this.authService.isLoggedIn;
+  isSticky = false;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.isSticky = window.scrollY > 100;
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.error('Logout failed', err);
+      },
+    });
+  }
 }

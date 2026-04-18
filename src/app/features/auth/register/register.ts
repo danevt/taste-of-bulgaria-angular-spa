da2b6aad31f1ea2c926a6d180e123498e9926e75
T.Danev <danevtm@gmail.com>
2026-04-18 20:58:32 +0300

    Add register form submission and authentication logic
    Handle registration logic in the register component:
    - Injected AuthService and Router for user registration and navigation.
    - Implemented onSubmit logic to process registration data.
    - Added validation checks for password matching.
    - Integrated error handling and field-touching logic on form failure.

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../../../core/services';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.markFormAsTouched(form);
+      return;
+    }
+
+    const { username, email, password, rePassword } = form.value;
+
+    if (password !== rePassword) {
+      console.error('Passwords do not match');
+      this.markFormAsTouched(form);
+      return;
+    }
+
+    this.authService.register(email, username, password).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => {
+        console.error('Registration failed', err);
+        this.markFormAsTouched(form);
+      },
+    });
+  }
+
+  private markFormAsTouched(form: NgForm): void {
+    Object.keys(form.controls).forEach((key) => {
+      form.controls[key].markAsTouched();
+    });
+  }
 }

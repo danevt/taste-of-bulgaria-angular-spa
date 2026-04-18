da053e7083957c0c8f6ce81719f5030744f991f3
T.Danev <danevtm@gmail.com>
2026-04-18 20:42:09 +0300

    Add login form logic and authentication
    Implement basic login functionality:
    - Added FormsModule for form handling.
    - Injected AuthService and Router for login and navigation.
    - Created onSubmit logic: redirects to /home on success.
    - Added error handling to highlight invalid fields on failure.

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../../../core/services';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.markFormAsTouched(form);
+      return;
+    }
+
+    const { email, password } = form.value;
+
+    this.authService.login(email, password).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => {
+        console.error('Login failed', err);
+        this.markFormAsTouched(form);
+      },
+    });
+  }
+
+  private markFormAsTouched(form: NgForm): void {
+    Object.keys(form.controls).forEach((key) => {
+      form.controls[key].markAsTouched();
+    });
+  }
 }

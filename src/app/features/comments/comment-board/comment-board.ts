82a7eecfa079dd9e0e98439d1bdadf833d790463
T.Danev <danevtm@gmail.com>
2026-04-18 22:57:00 +0300

    Add CommentBoard component stub
    Introduce a new CommentBoard component for the comments feature.

---
+++ src/app/features/comments/comment-board/comment-board.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-board',
+  imports: [],
+  templateUrl: './comment-board.html',
+  styleUrl: './comment-board.css',
+})
+export class CommentBoard {
+
+}

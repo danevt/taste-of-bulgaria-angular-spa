2dc87e34e344eef4a6878b16d4c9a96f534bfeff
T.Danev <danevtm@gmail.com>
2026-04-19 12:22:35 +0300

    Home: dynamic recent recipes and category links
    - Render categories with RouterLink and query params
    - Display latest recipes list with empty state
    - Load data from RecipeService on init
    - Enable navigation to recipe and category views

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { RecipeService } from '../../core/services';
+import { Recipe } from '../../models';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  private recipeService = inject(RecipeService);
 
+  latestRecipes: Recipe[] = [];
+
+  ngOnInit() {
+    this.recipeService.getLatestRecipes().subscribe({
+      next: (recipes) => (this.latestRecipes = recipes),
+      error: (err) => console.error('Failed to load latest recipes', err),
+    });
+  }
 }

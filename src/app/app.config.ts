0057647563fc64633a0a24bb931d0b1174f77759
T.Danev <danevtm@gmail.com>
2026-04-19 10:53:44 +0300

    Register Auth and Error interceptors in ApplicationConfig
    Configured HttpClient to use authentication and error interceptors for automatic cookie handling and global 401 error redirection.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,14 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authenticationInterceptor, errorInterceptor } from './core/interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authenticationInterceptor, errorInterceptor])),
   ],
 };

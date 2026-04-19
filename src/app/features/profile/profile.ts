bcb3b460a3116b75b927c5b1c9f9861206a7f50d
T.Danev <danevtm@gmail.com>
2026-04-19 10:01:30 +0300

    Add user profile UI, styles and logic
    - Add profile layout and styles
    - Display user info and list of user recipes
    - Handle empty state
    - Load user recipes on init

--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService, RecipeService } from '../../core/services';
+import { Recipe, User } from '../../models';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, DatePipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
+export class Profile implements OnInit {
+  private authService = inject(AuthService);
+  private recipeService = inject(RecipeService);
 
+  user: User | null = null;
+  userRecipes: Recipe[] = [];
+
+  ngOnInit() {
+    this.user = this.authService.currentUser();
+
+    if (this.user) {
+      this.recipeService.getAllRecipes().subscribe({
+        next: (recipes) => {
+          this.userRecipes = recipes.filter((recipe) =>
+            typeof recipe.userId === 'object'
+              ? recipe.userId._id === this.user!._id
+              : recipe.userId === this.user!._id,
+          );
+        },
+        error: (err) => console.error('Failed to load user recipes', err),
+      });
+    }
+  }
 }
